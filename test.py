e5cb7050ed200c0db7c99710e43b6c49b00b9805
mrnobody030 <fnsndsbnjues@gmail.com>
2024-12-06 02:20:31 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
+import subprocess as sp
+
+def searching_broser():
+  browsers = ["google-chrome", "chromium", "firefox", "safari", "microsoft-edge"]
+  for browser in browsers:
+    result = sp.run(["which", browser], stdout=sp.PIPE, stderr=sp.PIPE)
+    if result.returncode == 0:
+      print(result.stdout.decode().strip())
+      installed_browsers = browser 
+      print(browser)
+
 
 def test():
   print("hello world")
 
 if __name__ == "__main__":
   print("first")
-  test()
-
+  searching_broser()
 
