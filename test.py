60b558d3cb85299d6c0dfe8cb189c32d143fe503
mrnobody030 <fnsndsbnjues@gmail.com>
2024-12-06 02:29:36 +0330

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,14 @@ import subprocess as sp
 
 def searching_broser():
   browsers = ["google-chrome", "chromium", "firefox", "safari", "microsoft-edge"]
+  installed_browsers = {}
+
   for browser in browsers:
     result = sp.run(["which", browser], stdout=sp.PIPE, stderr=sp.PIPE)
-    if result.returncode == 0:
-      print(result.stdout.decode().strip())
-      installed_browsers = browser 
-      print(browser)
+    
+    installed_browsers[browser] =  result.stdout.decode().strip() if result.returncode == 0 else None
+
+    return {b: path for b, path in installed_browsers.items() if path}
 
 
 def test():
@@ -15,5 +17,4 @@ def test():
 
 if __name__ == "__main__":
   print("first")
-  searching_broser()
-
+  print(searching_broser())
