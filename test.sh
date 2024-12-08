fe0646bb4e2487e4aa7dd675069f063bcf39a44a
mrnobody030 <fnsndsbnjues@gmail.com>
2024-12-08 22:12:24 +0330

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-# Update package list
-sudo apt update -y
-
-# Install required dependencies
-sudo apt install -y wget gpg apt-transport-https software-properties-common
-
-# Add Microsoft's GPG key
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
-
-# Add the Microsoft Edge repository
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
-
-# Update package list again to include Edge repository
-sudo apt update -y
-
-# Install Microsoft Edge
-sudo apt install -y microsoft-edge-stable
+wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_118.0.2088.53-1_amd64.deb
 
 # Confirm installation
 microsoft-edge --version
