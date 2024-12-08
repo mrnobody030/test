a7521632f5422fc07f012e99a0e88baa7aab2d3b
mrnobody030 <fnsndsbnjues@gmail.com>
2024-12-08 21:57:07 +0330

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,3 @@ sudo apt update && \
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft.gpg > /dev/null && \
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge.list > /dev/null && \
 sudo apt update && sudo apt install -y microsoft-edge-stable
-sudo python3 test.py
