469283f55d4bfdddeee667520c33619887c1b58f
mrnobody030 <fnsndsbnjues@gmail.com>
2024-12-13 01:22:27 +0330

    Update test.sh
    testing playwright

--- test.sh
+++ test.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-
-wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_118.0.2088.53-1_amd64.deb
-
-# Confirm installation
-microsoft-edge --version
-
-python test.py
+pip install --upgrade pip
+pip install playwright
+playwright install
