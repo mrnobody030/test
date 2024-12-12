17bed5a42ebbcdbf7beaf1d5720e3dbbe63cfbf6
mrnobody030 <fnsndsbnjues@gmail.com>
2024-12-13 01:31:17 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,8 @@
-import subprocess as sp
+from playwright.sync_api import sync_playwright
 
-def searching_broser():
-  browsers = ["google-chrome", "chromium", "firefox", "safari", "microsoft-edge"]
-  installed_browsers = {}
-
-  for browser in browsers:
-    result = sp.run(["which", browser], stdout=sp.PIPE, stderr=sp.PIPE)
-    
-    installed_browsers[browser] =  result.stdout.decode().strip() if result.returncode == 0 else None
-
-    return {b: path for b, path in installed_browsers.items() if path}
-
-
-def test():
-  print("hello world")
-
-if __name__ == "__main__":
-  print("first")
-  print(searching_broser())
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto("http://google.com")
+    print(page.title())
+    browser.close()
